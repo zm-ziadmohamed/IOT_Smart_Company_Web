0fc6f2f919eca8c3e0f7b57ce28b23d1c1a5c4af
zm_ziadmohamed <ziadmohamedalking2004@gmail.com>
2024-08-28 01:08:38 +0300

    adding api

--- routes/employeeAttendance.js
+++ routes/employeeAttendance.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const { isLoggedIn } = require('../security')
 const employeeAttendanceModel = require('../models/attendance');
-
+const employeeModel = require('../models/employee')
 // Route to display attendance with employee data
 router.get('/attendance', isLoggedIn, async (req, res, next) => {
     try {
@@ -28,8 +28,23 @@ router.get('/attendance', isLoggedIn, async (req, res, next) => {
         next(error); // Pass the error to the error handler middleware
     }
 });
-
-
+router.post('/checkSecret',(req,res,next)=>{
+     employeeModel.findOne({secret : req.body.secret}).then(user => {
+        console.log(req.body)
+        if (user) {
+            res.json({msg : "correct"})
+        } else {
+            res.json({msg : "inncorrect"})
+        }
+     }).catch("error occured")
+})
+router.post('/recordAttendance',(req,res,next)=>{
+    (new employeeAttendanceModel({secret : req.body.secret})).save().then(employeeAttendance => {
+        res.json({msg : "done"})
+    }).catch(err => {
+        res.send({msg : "error occurred"})
+    })
+})
 
 
 
